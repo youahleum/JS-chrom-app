0cc9c7d0196c0702cdbe82c4541a4e2f7c312121
youahleum <dkfma6033@gmail.com>
2022-02-21 04:22:32 +0900

    weather.js 추가

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+//
+const API_KEY = "08cba0d3e18be6bfc91b755e72128e79";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("geo Error");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
